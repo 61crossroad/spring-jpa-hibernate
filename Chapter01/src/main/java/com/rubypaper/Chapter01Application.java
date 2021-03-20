997535b4c1ff1ac686229f7ec8cc8ab88a33cd38
61crossroad <61crossroad@gmail.com>
2021-03-20 19:07:15 +0900

    Simple Spring Boot Controller & VO

--- Chapter01/src/main/java/com/rubypaper/Chapter01Application.java
+++ Chapter01/src/main/java/com/rubypaper/Chapter01Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Chapter01Application {
-
+	// Write your code here
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter01Application.class, args);
 	}
